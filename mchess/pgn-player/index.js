e8747c2e8236e51e7f210a82463468425c172d24
Seth Pomerantz <seth@thebestfriendsgang.nyc>
2022-05-05 17:17:28 -0400

    working Randfom PGN

--- mchess/pgn-player/index.js
+++ mchess/pgn-player/index.js
@@ -1,95 +1,104 @@
-const WebSocket = require('ws')
-const url = 'ws://127.0.0.1:8001/ws'
-var connection = new WebSocket(url)
+const WebSocket = require('ws');
+
+const url = 'ws://127.0.0.1:8001/ws';
+const connection = new WebSocket(url);
 const readline = require('readline');
+
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
 
-
-
-const fs = require("fs");
-const path = require("path");
-
-
-
-
+const fs = require('fs');
+const path = require('path');
 
 function loadRandomPGN() {
+  const files = fs.readdirSync(path.join(process.cwd(), 'pgn'));
 
-    const files = fs.readdirSync(path.join(process.cwd(), "pgn"));
+  const max = files.length - 1;
+  const min = 0;
 
+  const index = Math.round(Math.random() * (max - min) + min);
+  const file = files[index];
 
-    let max = files.length - 1;
-    let min = 0;
+  console.log('Random PGN is', file);
 
-    let index = Math.round(Math.random() * (max - min) + min);
-    let file = files[index];
-
-    console.log("Random file is", file);
-
-    try {
-      const data = fs.readFileSync(path.join(process.cwd(),'pgn',file), 'utf8')
-      return data;
-    } catch (err) {
-      console.error(err)
-    }
-
+  try {
+    return fs.readFileSync(path.join(process.cwd(), 'pgn', file), 'utf8');
+  } catch (err) {
+    console.error(err);
+  }
 }
 
-
-
-
+const delta = 500;
+let lastKeypressTime = 0;
+const doubled = false;
 
 process.stdin.on('keypress', (str, key) => {
+  let thisKeypressTime = new Date();
+  console.log(`${thisKeypressTime - lastKeypressTime}`);
+
+  if (thisKeypressTime - lastKeypressTime <= delta) {
+    var doubled = true;
+    // optional - if we'd rather not detect a triple-press
+    // as a second double-press, reset the timestamp
+    thisKeypressTime = 0;
+  }
+  lastKeypressTime = thisKeypressTime;
+
   if (key.ctrl && key.name === 'c') {
     process.exit();
   } else {
-
     if (key.name === 'left') {
-        moveBackward();
+      moveBackward();
     } else if (key.name === 'right') {
-        moveForward();
+      moveForward();
     } else if (key.name === 'down') {
-        loadPGN();
+      loadPGN();
+      if (doubled == true) {
+        console.log('Move Start');
+        move_start();
+      }
     }
     console.log(`You pressed the "${key.name}" key`);
   }
 });
 console.log('Press any key...');
 
-
 connection.onopen = () => {
-    console.log('opened'); 
-}
- 
+  console.log('opened');
+};
+
 connection.onerror = (error) => {
-  console.log(`WebSocket error: ${JSON.stringify(error)}`)
+  console.log(`WebSocket error: ${JSON.stringify(error)}`);
+};
+
+function loadPGN() {
+  connection.send(JSON.stringify({
+    cmd: 'import_pgn',
+    pgn: loadRandomPGN(),
+    actor: 'WebAgent',
+  }));
 }
- 
 
-function loadPGN(data) {
-          connection.send(JSON.stringify({
-                  "cmd": "import_pgn",
-                  "pgn": loadRandomPGN(), 
-                  "actor": "WebAgent"
-                }                ))
+function move_start() {
+  connection.send(JSON.stringify({
+    cmd: 'move_start',
+    actor: 'WebAgent',
+  }));
 }
-
-
 function moveForward() {
-    connection.send(JSON.stringify(
-        {
-          "cmd": "move_forward",
-          "actor": "WebAgent"
-        }
-    ))
+  connection.send(JSON.stringify(
+    {
+      cmd: 'move_forward',
+      actor: 'WebAgent',
+    },
+  ));
 }
 
 function moveBackward() {
-    connection.send(JSON.stringify(
-        {
-          "cmd": "move_back",
-          "actor": "WebAgent"
-        }
-    ))
+  connection.send(JSON.stringify(
+    {
+      cmd: 'move_back',
+      actor: 'WebAgent',
+    },
+  ));
 }
